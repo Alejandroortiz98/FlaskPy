a3f0fb1c32d2a76d860ac376ca1a120bfefb03e4
Alejandro O <alex.ortiz98.garcia@gmail.com>
2020-03-02 10:21:58 -0600

    Added some stuff it has errors idk why

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask,render_template
 
 app = Flask(__name__)
 
+all_posts =[ #inicio diccionario de listas
+    {
+        'title': 'Post 1',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+
+    },
+    {
+        'title': 'Post 2',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+    }
+] #fin diccionario de listas
+
+
 @app.route('/')
 def index():
     return render_template('index.html') #Lo que sea que referencies aqui debe estar en la carpeta templates
+
+@app.route('/posts')
+def posrs():
+    return render_template('posts.html', posts=all_posts) #donde sea que se haya referenciado el html tambien puedes referenciar una variable
 
 @app.route('/home/<string:name>') #<DataType:variableName>
 def hello(name): #si planeas usar la variable de arriba pss asegurate que lo de () este igual a lo de <>
